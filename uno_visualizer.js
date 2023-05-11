b93a43beecec43ebf8b242a502ab8075bfd7a3b9
psolanky08 <75457120+psolanky08@users.noreply.github.com>
2023-05-11 11:14:13 -0400

    Add files via upload

--- uno_visualizer.js
+++ uno_visualizer.js
@@ -4,7 +4,165 @@ const deck = Deck
 const player1 = One
 const unogame = Game 
 const hand1 = player1.hand
-const url1 = 'https://media.istockphoto.com/id/485651937/photo/deck-of-uno-game-cards.jpg?s=612x612&w=0&k=20&c=UPAkLRxdHVyVyXJOW55iA-_Fg21BSIaZRVci-SOBx9c=' 
+const url1 = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fed3bb24-454f-4bdf-a721-6aa8f23e7cef/d9gnihf-ec16caeb-ec9c-4870-9480-57c7711d844f.png/v1/fill/w_486,h_759/uno_card_back_by_wackosamurai_d9gnihf-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzU5IiwicGF0aCI6IlwvZlwvZmVkM2JiMjQtNDU0Zi00YmRmLWE3MjEtNmFhOGYyM2U3Y2VmXC9kOWduaWhmLWVjMTZjYWViLWVjOWMtNDg3MC05NDgwLTU3Yzc3MTFkODQ0Zi5wbmciLCJ3aWR0aCI6Ijw9NDg2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.TMtpsV_KVjgkIMqwza3ooob2Xq5bihDuT0JVsOldrpA'; 
+
+const red0 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red0.png';
+const blue0 = 'https://www.shutterstock.com/shutterstock/photos/1934989541/display_1500/stock-photo-negotin-serbia-march-uno-playing-card-zero-in-blue-color-playing-card-isolated-1934989541.jpg';
+const yellow0 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow0.png';
+const green0 = 'https://www.shutterstock.com/shutterstock/photos/1934990045/display_1500/stock-photo-negotin-serbia-march-uno-playing-card-zero-in-blue-green-playing-card-isolated-1934990045.jpg';
+const red1 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red1.png';
+const blue1 = 'https://i.pinimg.com/474x/80/8a/e0/808ae01a2c8c8634b967c9a9ea59b1cf.jpg';
+const yellow1 = 'https://cdna.artstation.com/p/assets/images/images/043/685/602/large/nicholas-fox-uno-yellow.jpg?1637968813';
+const green1 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-green1.png';
+const red2 = 'https://www.shutterstock.com/shutterstock/photos/1935293165/display_1500/stock-photo-negotin-serbia-march-uno-playing-card-two-in-red-color-playing-card-isolated-on-1935293165.jpg';
+const blue2 = 'https://i.pinimg.com/474x/19/e4/4a/19e44a67eef087ce9b4ed590ddac5509.jpg'; 
+const yellow2 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow2.png';
+const green2 = 'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/75061/green-2-card-clipart-xl.png';
+const red3 = 'https://cdnb.artstation.com/p/assets/images/images/043/685/567/large/nicholas-fox-uno-red.jpg?1637968732';
+const blue3 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-blue3.png'; 
+const yellow3 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow3.png';
+const green3 = 'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/75062/green-3-card-clipart-md.png'; 
+const red4 = 'https://i.pinimg.com/474x/0f/09/54/0f09541f4405675c31ce6c97e13c237c.jpg'; 
+const blue4 = 'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/75050/blue-4-card-clipart-md.png'; 
+const yellow4 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow4.png'; 
+const green4 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-green4.png'; 
+const red5 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red5.png';
+const blue5 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-blue5.png'; 
+const yellow5 = 'https://i.pinimg.com/474x/58/24/f4/5824f495b04f358f9fa6a4c0cdaf3605.jpg'; 
+const green5 = 'https://i.pinimg.com/474x/2b/c1/2e/2bc12e7cfca167cd7d5633f14219d68c.jpg'; 
+const red6 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red6.png'; 
+const blue6 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-blue6.png';
+const yellow6 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow6.png';
+const green6 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-green6.png'; 
+const red7 = 'https://patana.fireflycloud.asia/resource.aspx?id=1385552'; 
+const blue7 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-blue7.png'; 
+const yellow7 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow7.png'; 
+const green7 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-green7.png'
+const red8 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red8.png'
+const blue8 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-blue8.png';
+const yellow8 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow8.png';
+const green8 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-green8.png'
+const red9 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red9.png';
+const yellow9 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-yellow9.png';
+const green9 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-green9.png';
+const blue9 = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-blue9.png';
+
+const red_cards= [red0, red1, red2, red3, red4, red5, red6, red7, red8, red9];
+const blue_cards = [blue0, blue1, blue2, blue3, blue4, blue5, blue6, blue7, blue8, blue9];
+const green_cards = [green0, green1, green2, green3, green4, green5, green6, green7, green8, green9];
+const yellow_cards = [yellow0, yellow1, yellow2, yellow3, yellow4, yellow5, yellow6, yellow7, yellow8, yellow9];
+
+const stage = new Stage()
+const theArray = instance.signature('NumberCard').atoms()
+const elementsField = instance.field('hand')
+new TextBox('hello!', {x: 50, y: 50}, 'black', 12)
+const newArray = []; 
+const myArr = []; 
+let numbers1 = One.hand.toString().match(/\d+/g);
+for (const card in theArray) { 
+    newArray.push(theArray[card])
+    myArr.push(card)
+    
+}
+let commonElements1 = myArr.filter(element => One.hand.toString().match(/\d+/g).includes(element));
+let colors1 = [];
+let ints1 = [];
+
+for (i = 0; i < commonElements1.length; i++) {
+  colors1.push(newArray[commonElements1[i]].color.toString())
+  ints1.push(newArray[commonElements1[i]].number.toString())
+}
+
+let commonElements2 = myArr.filter(element => Two.hand.toString().match(/\d+/g).includes(element));
+let colors2 = [];
+let ints2 = [];
+
+for (i = 0; i < commonElements2.length; i++) {
+  colors2.push(newArray[commonElements2[i]].color.toString())
+  ints2.push(newArray[ commonElements2[i]].number.toString())
+}
+
+stage.add(new TextBox({
+  text: commonElements2,
+  coords: {x:100, y:100},
+  color: 'black',
+  fontSize: 16}))
+stage.render(svg, document)
+
+
+
+
+
+//Get all the data out of Sterling
+// var stage =  document.getElementsByClassName("script-stage")[1];
+// const allinst = instances.map(instanceToGraph)
+// const numinst = instances.length - 1
+// var maindiv = getElem("stagediv","div")
+// stage.appendChild(maindiv); 
+// const stage = new Stage()
+// const theArray = instance.signature('NumberCard').atoms()[0]
+// const elementsField = instance.field('hand')
+// new TextBox('hello!', {x: 50, y: 50}, 'black', 12)
+
+// stage.add(new TextBox({
+//   text: `${theArray.join(elementsField)}`, 
+//   coords: {x:100, y:100},
+//   color: 'black',
+//   fontSize: 16}))
+// stage.render(svg, document)
+// function instanceToGraph(inst) { 
+//   //NumberCards
+//   const allcards = inst.signature("NumberCard").atoms(true);
+//   for (const card in allcards) {
+//     console.log("card: ", card)
+//     if (card in One.hand) {
+//       console.log("Yes")
+//     }
+//   }
+//   // const p1cards = inst.signature("hand").atoms(true); 
+//   //document.write(p1cards)
+//   //properties of cards 
+//   const colors = inst.field('color'); 
+//   const numbers = inst.field('number'); 
+
+//   // function getProcess(p) { 
+//   //   const prpg = p.join(proc_ptb).join(mapping).tuples().map(e => {return e.atoms()[0].id()})
+//   //               .reduce((a,v) => a +"," + v,"")  
+//   //   const perms = p.join(proc_ptb.join(permissions)).tuples().map(e => {return [e.atoms()[0].id(),e.atoms()[1].id()]})
+//   //                 .map(e => "(" + e[0] +","+e[1]+"),").reduce((a,v) => a + "\n " + v , "")
+                                                          
+    
+//   //   return {
+//   //   pname : p.id() , 
+//   //   proc_id : p.join(pid).tuples()[0].atoms()[0].id() , 
+//   //   proc_tbl : p.join(proc_ptb).tuples().map(e => {return e.atoms()[0].id()}).reduce((a,v) => a + " " + v , "") , 
+//   //   proc_pages: prpg , 
+//   //   perms: perms,
+//   //   proc_state: p.join(proc_st).tuples().map(e => {return e.atoms()[0].id()}).reduce((a,v) => a + " " + v , "")
+//   // }
+//   // }
+//   // const allprocs = procs.map(getProcess) ; 
+//   // function getPages(mp) {
+//   //   const pgdr = mp.atoms()[1].id()  ; 
+//   //   const pgArr = new Array() ; 
+//   //   for(const proc of allprocs) { 
+//   //     if(proc.proc_pages.includes(pgdr) ){
+//   //       pgArr.push(proc.pname)
+//   //     }
+//   //   }
+//   //   return {
+//   //     pagename:mp.atoms()[0].id(),
+//   //     addr: pgdr ,
+//   //     page_procs:pgArr
+//   //   }
+//   // }
+//   // const pgs = padr.tuples().map(getPages) ; 
+//   // return {
+//   //   process:allprocs,
+//   //   pages:pgs
+//   // } 
+  
+// }
 
 function make_deck(url) {
   const img = document.createElement('img');
@@ -21,29 +179,68 @@ function make_deck(url) {
 
 
 
-function trial() { 
+function trial(hand) { 
     // const img1 = document.createElement('img')
     // const img2 = document.createElement('img')
     // const img3 = document.createElement('img')
     // const img4 = document.createElement('img')
     image_container = document.createElement("div")
-    const hand1 = One.hand
-    const hand2 = Two.hand
-    for (let i = 0; i < 4; i++) {
-      const color = One.hand[i].color
-      const num = One.hand[i].number //not used rn 
-      var imageStyle = "width: 100px; height: 100px; background-color: gray; margin: 10px; float: left;";
-      const img = document.createElement('img')
-      img.setAttribute("style", imageStyle)
-      if (i == 0){ 
-        img.src = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red1.png'
-      } else if (i == 1) {
-        img.src = 'https://i.pinimg.com/originals/80/8a/e0/808ae01a2c8c8634b967c9a9ea59b1cf.png'
-      } else if (i == 2) { 
-        img.src = 'https://media.baamboozle.com/uploads/images/182571/1613773171_86287.png'
-      } else if (i == 3) {
-        img.src = 'https://dbdzm869oupei.cloudfront.net/img/vinylrugs/preview/55306.png'
+
+    // for (let i = 0; i < 4; i++) {
+    //   const color = One.hand[i].color
+    //   const num = One.hand[i].number //not used rn 
+    //   var imageStyle = "width: 100px; height: 100px; background-color: gray; margin: 10px; float: left;";
+    //   const img = document.createElement('img')
+    //   img.setAttribute("style", imageStyle)
+    if (hand == 1) {
+      for (i = 0; i < colors1.length; i++) {
+        var imageStyle = "width: 100px; height: 100px; background-color: gray; margin: 10px; float: left;";
+        const img = document.createElement('img')
+        img.setAttribute("style", imageStyle)
+        if (colors1[i] == "Red0") {
+          img.src = red_cards[ints1[i]]
+        }
+        if (colors1[i] == "Blue0") {
+          img.src = blue_cards[ints1[i]]
+        }
+        if (colors1[i] == "Yellow0") {
+          img.src = yellow_cards[ints1[i]]
+        }
+        if (colors1[i] == "Green0") {
+          img.src = green_cards[ints1[i]]
+        }
+        image_container.appendChild(img)
       }
+    }
+    if (hand == 2) {
+      for (i = 0; i < colors2.length; i++) {
+        var imageStyle = "width: 100px; height: 100px; background-color: gray; margin: 10px; float: left;";
+        const img = document.createElement('img')
+        img.setAttribute("style", imageStyle)
+        if (colors2[i] == "Red0") {
+          img.src = red_cards[ints2[i]]
+        }
+        if (colors2[i] == "Blue0") {
+          img.src = blue_cards[ints2[i]]
+        }
+        if (colors2[i] == "Yellow0") {
+          img.src = yellow_cards[ints2[i]]
+        }
+        if (colors2[i] == "Green0") {
+          img.src = green_cards[ints2[i]]
+        }
+        image_container.appendChild(img)
+      }
+    }
+      // if (i == 0){ 
+      //   img.src = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red1.png'
+      // } else if (i == 1) {
+      //   img.src = 'https://i.pinimg.com/originals/80/8a/e0/808ae01a2c8c8634b967c9a9ea59b1cf.png'
+      // } else if (i == 2) { 
+      //   img.src = 'https://media.baamboozle.com/uploads/images/182571/1613773171_86287.png'
+      // } else if (i == 3) {
+      //   img.src = 'https://dbdzm869oupei.cloudfront.net/img/vinylrugs/preview/55306.png'
+      // }
     //   if (color == Red) { 
     //     img.src = 'https://unocardinfo.victorhomedia.com/graphics/uno_card-red1.png'
     //   } else if (color == Blue) {
@@ -53,27 +250,27 @@ function trial() {
     //   } else if (color == Yellow) {
     //     img.src = 'https://dbdzm869oupei.cloudfront.net/img/vinylrugs/preview/55306.png'
     //   }
-      image_container.appendChild(img)
-      //document.write(hand1)
-      for (const ind in hand1.tuples()) {
-        const cards = hand1.tuples()[ind]
-        document.write(cards)
-        // for (const ind2 in cards.tuples()) {
-        //   document.write(typeof(cards.tuples()[0]))
-        // }     
-      }
+      // image_container.appendChild(img)
+      // //document.write(hand1)
+      // for (const ind in hand1.tuples()) {
+      //   const cards = hand1.tuples()[ind]
+      //   //document.write(cards)
+      //   // for (const ind2 in cards.tuples()) {
+      //   //   document.write(typeof(cards.tuples()[0]))
+      //   // }     
+      // }
      
-    }
+    //}
     return image_container
   }
 function make_blank() {
-  const div = make_deck('https://media.istockphoto.com/id/485651937/photo/deck-of-uno-game-cards.jpg?s=612x612&w=0&k=20&c=UPAkLRxdHVyVyXJOW55iA-_Fg21BSIaZRVci-SOBx9c=' )
+  const div = make_deck('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/fed3bb24-454f-4bdf-a721-6aa8f23e7cef/d9gnihf-ec16caeb-ec9c-4870-9480-57c7711d844f.png/v1/fill/w_486,h_759/uno_card_back_by_wackosamurai_d9gnihf-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzU5IiwicGF0aCI6IlwvZlwvZmVkM2JiMjQtNDU0Zi00YmRmLWE3MjEtNmFhOGYyM2U3Y2VmXC9kOWduaWhmLWVjMTZjYWViLWVjOWMtNDg3MC05NDgwLTU3Yzc3MTFkODQ0Zi5wbmciLCJ3aWR0aCI6Ijw9NDg2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.TMtpsV_KVjgkIMqwza3ooob2Xq5bihDuT0JVsOldrpA' )
   div.style.opacity = '80%'
   return div
 }
 
-function make_cards() {
-    const div = trial()
+function make_cards(handnum) {
+    const div = trial(handnum)
     div.style.opacity = '70%'
     return div 
 }
@@ -95,8 +292,9 @@ box4.setAttribute("style", boxStyle);
 // Add the boxes to the new window
 // popup.document.body.appendChild(box1);
 // popup.document.body.appendChild(box2);
-popup.document.body.appendChild(make_cards())
+popup.document.body.appendChild(make_cards(1))
+const emptyParagraph = popup.document.createElement("p");
+emptyParagraph.textContent = "";
+popup.document.body.appendChild(emptyParagraph);
 popup.document.body.appendChild(make_blank())
-// popup.document.body.appendChild(box3);
-// popup.document.body.appendChild(box4);
-popup.document.body.appendChild(make_cards())
+popup.document.body.appendChild(make_cards(2))
